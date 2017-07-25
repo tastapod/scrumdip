1d13b2cd954ebbe09d6a73d3b01d9726b0d30f12
Dan North <dan@dannorth.net>
2017-07-25 17:11:27 +0100

    create the database

---
+++ database.py
@@ -0,0 +1,41 @@
+import sqlite3
+import os.path
+from contextlib import closing
+
+DEFAULT_DB_FILE = 'scrumdip.db'
+
+
+def connect():
+    # fallback DB file based on location of the current source file
+    here = os.path.abspath(os.path.dirname(__file__))
+    fallback_file = os.path.join(here, DEFAULT_DB_FILE)
+
+    # use DB file location from environment or fall back
+    db_file = os.environ.get('DB_FILE', fallback_file)
+    return sqlite3.connect(db_file)
+
+
+def check_or_create_table():
+    with closing(connect()) as conn:
+        conn.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS cert_counts (
+                id INTEGER PRIMARY KEY,
+                cert TEXT,
+                count INTEGER,
+                created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            '''
+        )
+        conn.commit()
+
+
+def insert_results(cert, count):
+    with closing(connect()) as conn:
+        conn.execute(
+            '''
+            INSERT INTO cert_counts (cert, count)
+            VALUES (?, ?)
+            ''',
+            (cert, count))
+        conn.commit()
