eb96a6a99ec4e91af2f579fdcfd1e9ddff728d24
Dan North <dan@dannorth.net>
2017-08-01 13:18:49 +0100

    create separate tables for latest and historic counts

--- database.py
+++ database.py
@@ -10,7 +10,13 @@ def check_or_create_table():
     with closing(connect()) as conn, closing(conn.cursor()) as cur:
         cur.execute(
             '''
-            CREATE TABLE IF NOT EXISTS cert_counts (
+            CREATE TABLE IF NOT EXISTS cert_history (
+                id SERIAL PRIMARY KEY,
+                cert VARCHAR(10) NOT NULL,
+                count INTEGER NOT NULL,
+                created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            CREATE TABLE IF NOT EXISTS cert_latest (
                 id SERIAL PRIMARY KEY,
                 cert VARCHAR(10) NOT NULL,
                 count INTEGER NOT NULL,
@@ -25,23 +31,30 @@ def insert_results(cert, count):
     with closing(connect()) as conn, closing(conn.cursor()) as cur:
         cur.execute(
             '''
-            INSERT INTO cert_counts (cert, count)
-            VALUES (%s, %s);
+            INSERT INTO cert_history (cert, count)
+            VALUES (%(cert)s, %(count)s);
+
+            DELETE FROM cert_latest where cert = %(cert)s;
+            INSERT INTO cert_history (cert, count)
+            VALUES (%(cert)s, %(count)s);
             ''',
-            (cert, count))
+            dict(cert=cert, count=count))
         conn.commit()
 
 
 def latest_count(cert):
     with closing(connect()) as conn, closing(conn.cursor()) as cur:
         cur.execute(
-            '''
-            SELECT count, created FROM cert_counts
-            WHERE cert = %s
-            ORDER BY created DESC
-            LIMIT 1;
-            ''',
+            '''SELECT count FROM cert_latest WHERE cert = %s; ''',
             (cert,)
         )
         row = cur.fetchone()
         return row[0] if row else 0
+
+
+def latest_counts():
+    with closing(connect()) as conn, closing(conn.cursor()) as cur:
+        cur.execute(
+            '''SELECT cert, count, created FROM cert_latest; ''',
+        )
+        return = cur.fetchall()
