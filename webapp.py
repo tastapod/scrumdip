ca1a8f8e72e330bb50484417b6f51f97e8bfb5bd
Dan North <dan@dannorth.net>
2017-08-01 14:39:59 +0100

    get basic webapp working - w00t!

--- webapp.py
+++ webapp.py
@@ -1,25 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
+
 import database
+import fetch
 
 app = Flask(__name__)
+
 
 @app.route("/hello")
 def index():
     return render_template("index.html")
 
-@app.route("/search", methods=['POST'])
-def search():
-    cert = request.form['cert']
-    print('Searching for cert ' + cert)
-    count = database.latest_count(cert)
-    
-    return '''
-    <html>
-    <head><title>ScrumDip results for {0}</title>
-    </head>
-    <body>
-    <h1>Results for {0}</h1>
-    <p>We found <strong>{1}</strong> people with a <strong>{0}</strong> certificate.</p>
-    </body>
-    </html>
-    '''.format(cert, count)
+
+@app.route("/update", methods=["POST"])
+def update_counts():
+    database.check_or_create_tables()
+    counts = fetch.get_all_counts()
+    print(counts)
+    for cert, count in counts:
+        database.insert_results(cert, count)
+
+    return redirect(url_for("latest_counts")), 302
+
+
+@app.route("/latest")
+def latest_counts():
+    counts = database.latest_counts()
+    counts = [(cert, counts, ts.date()) for cert, counts, ts in counts]
+    print(counts)
+    return render_template("latest_counts.html", counts=counts)
