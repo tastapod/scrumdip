66cf04480087caa969f9c4bcc83da825fe0ccbbf
Dan North <dan@dannorth.net>
2017-08-01 13:19:29 +0100

    fetch all counts simultaneously

--- fetch.py
+++ fetch.py
@@ -1,36 +1,12 @@
 import re
 
 import requests
-
-'''
-https://www.scrumalliance.org/community/certificant-directory.aspx
-?firstname=
-&lastname=
-&email=
-&location=
-&company=
-&csm=False
-&csd=True
-&csp=False
-&cspo=False
-&cst=False
-&ctc=False
-&cec=False
-&advcsm=False
-&advcspo=False
-&cspsm=False
-&csppo=False
-&educator=False
-&rep=False
-&author=False
-&page=1
-&orderby=
-&sortdir=
-'''
+from threading import Thread
+from collections import deque
 
 DIRECTORY_URL = 'https://www.scrumalliance.org/community/certificant-directory.aspx'
 
-SEARCH = re.compile(r'Displaying results \d+-\d+ \(of (\d+)\)')
+CERT_COUNT_RE = re.compile(r'Displaying results \d+-\d+ \(of (\d+)\)')
 
 CERTS = [
     'csm', 'csd', 'csp', 'cspo', 'cst', 'ctc', 'cec',
@@ -49,9 +25,8 @@ PARAMS = dict(
 )
 
 
-def extract_total(html):
-    '''scan html for total value'''
-    match = SEARCH.search(html)
+def extract_count(html):
+    match = CERT_COUNT_RE.search(html)
     return int(match.group(1)) if match is not None else 0
 
 
@@ -62,12 +37,35 @@ def get_page(cert):
     return requests.post(DIRECTORY_URL, params=args)
 
 
-def get_total(cert):
+def get_count(cert):
     res = get_page(cert)
-    return extract_total(res.text) if res.ok else 0
+    return extract_count(res.text) if res.ok else 0
+
+
+def get_all_counts():
+    results = deque()
+    threads = []
+
+    def run(cert):
+        print("Going after " + cert)
+        count = get_count(cert)
+        print("{} has {} people".format(cert, count))
+        results.append((cert, count))
+
+    for cert in CERTS:
+        print("Starting thread for " + cert)
+        t = Thread(name=cert, target=run, args=(cert,))
+        t.start()
+        threads.append(t)
+
+    print("Waiting for threads")
+    for t in threads:
+        t.join()
+        print("Thread {} finished".format(t.name))
+
+    # At this point all the answers are in the results queue
+    return list(results)
 
 
 if __name__ == '__main__':
-    print(', '.join(CERTS))
-    cert = input("Enter certificate type: ")
-    print(cert, get_total(cert))
+    print(get_all_counts())
